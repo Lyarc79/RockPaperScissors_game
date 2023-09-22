75198aa00e93803e6f095dc931c55179b79b16a3
Lyarc79 <141921696+Lyarc79@users.noreply.github.com>
2023-09-22 11:28:13 +0200

    Adding some pseudocode

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
 
+// This are 2 global variables to track score
 let computerScore = 0;
 let playerScore = 0;
 
-
-
+// This function is an array that gets a random item for the console
 function getComputerChoice(){
     const choices = ["rock", "paper", "scissors"];
     computerChoice = choices[Math.floor(Math.random() * choices.length)];
     return computerChoice;
 }
 
+// This is a function for the user to write his choice
 function getPlayerChoice(){
     playerChoice = prompt ("rock, paper or scissors?").toLowerCase();
     return playerChoice;
 }
 
+// These are variables for the playround function
 let tie = "It's a tie!";
 let playerWin = "You win!";
 let computerWin = "The computer wins!";
 
+// Function that plays a round and displays the winner
 function playRound (playerSelection, computerSelection){
 
     if (playerSelection === computerSelection){
@@ -40,19 +43,23 @@ function playRound (playerSelection, computerSelection){
         }
 }
 
+// Storing the choices functions into variables
 const playerSelection = getPlayerChoice();
 const computerSelection = getComputerChoice();
+// Storing the playRound function to make it shorter
 let result = playRound(playerSelection, computerSelection);
 
+// Function that should display at the end when player or computer reaches 5 points.
 function checkWinner(){
-    if (playerWin === 5){
+    if (playerScore === 5){
         return "Player win's the game"
     }
-    else if (computerWin === 5){
+    else if (computerScore === 5){
         return "Computer win's the game"
     }
 }
 
+// Function to keep track of the score and update it every round
 function updateScore(){
     if (result == playerWin){
         playerScore++;
@@ -67,7 +74,7 @@ function updateScore(){
 }
 
 // Game function still working wrong.
-
+// Loop should be alright but the console display isn't the desired.
 function game(){
 
     for (let i = 0; i <= 5; i++){
